e69291b577404d69c2d687f496b1e218e7e0f3ce
qinchunbang <qcb@efanghang.com>
2018-10-27 17:26:33 +0800

    添加首页

--- FrontEnd/js/public.js
+++ FrontEnd/js/public.js
@@ -1,8 +1,19 @@
-var constant={
-    host:"http://localhost:8080"
+var constant = {
+    host: "http://localhost:8080",
+    userId: "userId",
+    token: "token"
+};
+
+var app={
+    //获取axios对象
+    getAxios: function () {
+        return axios.create({
+            baseURL: constant.host
+        });
+    }
 };
 
 //字符串左右去空
-String.prototype.trim=function(){
+String.prototype.trim = function () {
     return this.replace(/(^\s*)|(\s*$)/g, "");
 }
